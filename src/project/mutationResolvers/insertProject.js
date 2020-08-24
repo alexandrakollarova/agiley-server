d47bcecd14dbc0554b05ac7187b1156b1a3d224b
Alexandra Kollarova <alexandrakollarova91@gmail.com>
2020-08-24 13:59:20 -0400

    debugging

--- src/project/mutationResolvers/insertProject.js
+++ src/project/mutationResolvers/insertProject.js
@@ -3,13 +3,13 @@ export default async (__, args, cxt) => {
     const projectInfo = {
       title: args.request.title
     }
+    console.log('CONTEXT', cxt)
 
     const project = await cxt.project.insertProject(projectInfo)
 
     cxt.publisher.publish(cxt.SUBSCRIPTION_CONSTANTS.PROJECT_ADDED, {
       projectAdded: project
     })
-
     return project
   } catch (e) {
     console.log('error =>', e)
